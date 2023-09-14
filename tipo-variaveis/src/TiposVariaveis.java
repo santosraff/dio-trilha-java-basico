66af7b37b023a023a412e5297ba596cdc919348e
Rafael Santos <rafoliael@gmail.com>
2023-09-14 19:39:40 -0300

    feat: estudo sobre diferentes tipos de variáveis

--- tipo-variaveis/src/TiposVariaveis.java
+++ tipo-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMedio = 2500;
+
+        // casting
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroNormal2 = (short) numeroNormal;
+
+        final String VARIAVEL_CONSTANTE = "Constante";
     }
 }
